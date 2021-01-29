ce90e1662864b9ac10805ad51fb694802c8ae533
lane yang <git@zilingyang.com>
2021-01-29 14:07:03 -0800

    client post info to server

--- src/main/java/com/laioffer/job/servlet/ExampleBookServlet.java
+++ src/main/java/com/laioffer/job/servlet/ExampleBookServlet.java
@@ -1,11 +1,17 @@
 package com.laioffer.job.servlet;
 
+
+import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
 
 @WebServlet(name = "ExampleBookServlet" /*value = "/ExampleBookServlet"*/, urlPatterns = {"/example_book"})
 public class ExampleBookServlet extends HttpServlet {
@@ -32,6 +38,32 @@ public class ExampleBookServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        JSONObject jsonRequest = new JSONObject(IOUtils.toString(request.getReader()));
+        String title = jsonRequest.getString("title");
+        String author = jsonRequest.getString("author");
+        String date = jsonRequest.getString("date");
+        float price = jsonRequest.getFloat("price");
+        String currency = jsonRequest.getString("currency");
+        int pages = jsonRequest.getInt("pages");
+        String series = jsonRequest.getString("series");
+        String language = jsonRequest.getString("language");
+        String isbn = jsonRequest.getString("isbn");
+
+        System.out.println("Title is: " + title);
+        System.out.println("Author is: " + author);
+        System.out.println("Date is: " + date);
+        System.out.println("Price is: " + price);
+        System.out.println("Currency is: " + currency);
+        System.out.println("Pages is: " + pages);
+        System.out.println("Series is: " + series);
+        System.out.println("Language is: " + language);
+        System.out.println("ISBN is: " + isbn);
+
+
+        response.setContentType("application/json");
+        JSONObject jsonResponse = new JSONObject();
+        jsonResponse.put("status", "ok");
+        response.getWriter().print(jsonResponse);
 
     }
 }
