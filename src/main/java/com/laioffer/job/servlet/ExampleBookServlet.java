01076b316fe7667e479fdcc3ffab0d5260164c2f
lane yang <git@zilingyang.com>
2021-01-29 13:41:17 -0800

    client get from server

--- src/main/java/com/laioffer/job/servlet/ExampleBookServlet.java
+++ src/main/java/com/laioffer/job/servlet/ExampleBookServlet.java
@@ -1,5 +1,7 @@
 package com.laioffer.job.servlet;
 
+import org.json.JSONObject;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -14,6 +16,18 @@ public class ExampleBookServlet extends HttpServlet {
         String category = request.getParameter("category");
         System.out.println("Keyword is:"+ keyword);
         System.out.println("Category is"+category);
+        response.setContentType("application/json");
+        JSONObject json = new JSONObject();
+        json.put("title", "Harry Potter and the Sorcerer's Stone");
+        json.put("author", "JK Rowling");
+        json.put("date", "October 1, 1998");
+        json.put("price", 11.99);
+        json.put("currency", "USD");
+        json.put("pages", 309);
+        json.put("series", "Harry Potter");
+        json.put("language", "en_US");
+        json.put("isbn", "0590353403");
+        response.getWriter().print(json);
     }
 
     @Override
