7b3ef719b7fe411cffeea1235305ab32b0bb2bf6
lane yang <git@zilingyang.com>
2021-01-28 18:27:33 -0800

    test

--- src/main/java/com/laioffer/job/servlet/TestServlet.java
+++ src/main/java/com/laioffer/job/servlet/TestServlet.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class TestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().print("hello world?" 
+        response.getWriter().print("helo world?"
                 );
 
     }
