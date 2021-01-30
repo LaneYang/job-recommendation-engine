52acd5816e8e8600f7bf9d24be90de21c62778dc
lane yang <git@zilingyang.com>
2021-01-29 18:48:48 -0800

    clean up unefficient implementations

--- src/main/java/com/laioffer/job/servlet/ExampleBookServlet.java
+++ src/main/java/com/laioffer/job/servlet/ExampleBookServlet.java
@@ -1,6 +1,8 @@
 package com.laioffer.job.servlet;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.job.entity.ExampleBook;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 
@@ -13,58 +15,69 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
-@WebServlet(name = "ExampleBookServlet" /*value = "/ExampleBookServlet"*/, urlPatterns = {"/example_book"})
+@WebServlet(name = "ExampleBookServlet" /*value = "/ExampleBookServlet"*/, urlPatterns = {
+    "/example_book"})
 public class ExampleBookServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().print("This is the example book servlet");
-        String keyword = request.getParameter("keyword");
-        String category = request.getParameter("category");
-        System.out.println("Keyword is:"+ keyword);
-        System.out.println("Category is"+category);
-        response.setContentType("application/json");
-        JSONObject json = new JSONObject();
-        json.put("title", "Harry Potter and the Sorcerer's Stone");
-        json.put("author", "JK Rowling");
-        json.put("date", "October 1, 1998");
-        json.put("price", 11.99);
-        json.put("currency", "USD");
-        json.put("pages", 309);
-        json.put("series", "Harry Potter");
-        json.put("language", "en_US");
-        json.put("isbn", "0590353403");
-        response.getWriter().print(json);
-    }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //manually parsing Json, not efficient
-        JSONObject jsonRequest = new JSONObject(IOUtils.toString(request.getReader()));
-        String title = jsonRequest.getString("title");
-        String author = jsonRequest.getString("author");
-        String date = jsonRequest.getString("date");
-        float price = jsonRequest.getFloat("price");
-        String currency = jsonRequest.getString("currency");
-        int pages = jsonRequest.getInt("pages");
-        String series = jsonRequest.getString("series");
-        String language = jsonRequest.getString("language");
-        String isbn = jsonRequest.getString("isbn");
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    response.setContentType("application/json");
+    ObjectMapper objectMapper = new ObjectMapper();
+    ExampleBook testBook = new ExampleBook("痛并快乐着", "白岩松", "1.1.2021",
+        "usd", "人生", "chinese",
+        "123", (float) 20.35, 500);
+    response.getWriter().print(objectMapper.writeValueAsString(testBook));
 
-        System.out.println("Title is: " + title);
-        System.out.println("Author is: " + author);
-        System.out.println("Date is: " + date);
-        System.out.println("Price is: " + price);
-        System.out.println("Currency is: " + currency);
-        System.out.println("Pages is: " + pages);
-        System.out.println("Series is: " + series);
-        System.out.println("Language is: " + language);
-        System.out.println("ISBN is: " + isbn);
+//    response.getWriter().print("This is the example book servlet");
+//    String keyword = request.getParameter("keyword");
+//    String category = request.getParameter("category");
+//    System.out.println("Keyword is:" + keyword);
+//    System.out.println("Category is" + category);
+//    response.setContentType("application/json");
+//    JSONObject json = new JSONObject();
+//    json.put("title", "Harry Potter and the Sorcerer's Stone");
+//    json.put("author", "JK Rowling");
+//    json.put("date", "October 1, 1998");
+//    json.put("price", 11.99);
+//    json.put("currency", "USD");
+//    json.put("pages", 309);
+//    json.put("series", "Harry Potter");
+//    json.put("language", "en_US");
+//    json.put("isbn", "0590353403");
+//    response.getWriter().print(json);
+  }
 
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+//        //manually parsing Json, not efficient
+//        JSONObject jsonRequest = new JSONObject(IOUtils.toString(request.getReader()));
+//        String title = jsonRequest.getString("title");
+//        String author = jsonRequest.getString("author");
+//        String date = jsonRequest.getString("date");
+//        float price = jsonRequest.getFloat("price");
+//        String currency = jsonRequest.getString("currency");
+//        int pages = jsonRequest.getInt("pages");
+//        String series = jsonRequest.getString("series");
+//        String language = jsonRequest.getString("language");
+//        String isbn = jsonRequest.getString("isbn");
+//
+//        System.out.println("Title is: " + title);
+//        System.out.println("Author is: " + author);
+//        System.out.println("Date is: " + date);
+//        System.out.println("Price is: " + price);
+//        System.out.println("Currency is: " + currency);
+//        System.out.println("Pages is: " + pages);
+//        System.out.println("Series is: " + series);
+//        System.out.println("Language is: " + language);
+//        System.out.println("ISBN is: " + isbn);
+//
+//
+//        response.setContentType("application/json");
+//        JSONObject jsonResponse = new JSONObject();
+//        jsonResponse.put("status", "ok");
+//        response.getWriter().print(jsonResponse);
 
-        response.setContentType("application/json");
-        JSONObject jsonResponse = new JSONObject();
-        jsonResponse.put("status", "ok");
-        response.getWriter().print(jsonResponse);
-
-    }
+  }
 }
