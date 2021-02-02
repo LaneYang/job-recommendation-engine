7fa09c534ad423a1f5b2cfab76ddbe9a236a6d2e
lane yang <git@zilingyang.com>
2021-02-02 11:14:43 -0800

    building a responsehandler to retrive needed info from Github job api then deliver it to our own client

--- src/main/java/com/laioffer/job/external/GithubClient.java
+++ src/main/java/com/laioffer/job/external/GithubClient.java
@@ -2,27 +2,52 @@ package com.laioffer.job.external;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 public class GithubClient {
 
+
   private static final String URL_TEMPLATE = "https://jobs.github.com/positions.json?description=%s&lat=%s&long=%s";
+  //set default search word if the user left it empty
   private static final String DEFAULT_KEYWORD = "developer";
 
-  public String search(double latitude, double longitude, String keyword){
-    if (keyword == null){
+  public String search(double latitude, double longitude, String keyword) {
+    if (keyword == null) {
       keyword = DEFAULT_KEYWORD;
     }
     //process chars like spaces
     try {
-      keyword= URLEncoder.encode(keyword,"UTF-8" );
+      keyword = URLEncoder.encode(keyword, "UTF-8");
     } catch (UnsupportedEncodingException e) {
       e.printStackTrace();
     }
-
-    String url = String.format(URL_TEMPLATE,keyword,latitude,longitude);
+    //format the url using our template above
+    String url = String.format(URL_TEMPLATE, keyword, latitude, longitude);
     CloseableHttpClient httpClient = HttpClients.createDefault();
+//create a response handler: handles response from github api and process it to our own client
+    ResponseHandler<String> responseHandler = (response) -> {
+      if (response.getStatusLine().getStatusCode() != 200) {
+        return "";
+      }
 
+      HttpEntity httpEntity = response.getEntity();
+      if(httpEntity ==null){
+        return "";
+      }
+      return EntityUtils.toString(httpEntity);
+
+    };
+    try {
+      return httpClient.execute(new HttpGet(url),responseHandler);
+    }catch (Exception e){
+      e.printStackTrace();
+    }
+    return "";
   }
 }
--- src/main/java/com/laioffer/job/servlet/SearchServlet.java
+++ src/main/java/com/laioffer/job/servlet/SearchServlet.java
@@ -1,5 +1,6 @@
 package com.laioffer.job.servlet;
 
+import com.laioffer.job.external.GithubClient;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -11,7 +12,11 @@ public class SearchServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-    response.getWriter().write("test done");
+   // response.getWriter().write("test done");
+    double latitude = Double.parseDouble(request.getParameter("lat"));
+    double longitude = Double.parseDouble(request.getParameter("lon"));
+
+    GithubClient githubClient = new GithubClient();
   }
 
   @Override
