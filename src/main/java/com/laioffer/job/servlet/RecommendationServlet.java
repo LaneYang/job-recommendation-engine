58e7dd837d512e20e0d49828a6a1f8fbe46ad2b7
lane yang <git@zilingyang.com>
2021-02-14 13:35:01 -0800

    recommendation feature implementation

--- src/main/java/com/laioffer/job/servlet/RecommendationServlet.java
+++ src/main/java/com/laioffer/job/servlet/RecommendationServlet.java
@@ -1,7 +1,10 @@
 package com.laioffer.job.servlet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.job.entity.Item;
 import com.laioffer.job.entity.ResultResponse;
+import com.laioffer.job.recommendation.Recommendation;
+import java.util.List;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -14,7 +17,7 @@ public class RecommendationServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-    response.getWriter().write("test done");
+    response.setContentType("application/json");
     ObjectMapper mapper = new ObjectMapper();
     HttpSession session = request.getSession(false);
     if (session == null) {
@@ -22,6 +25,14 @@ public class RecommendationServlet extends HttpServlet {
       mapper.writeValue(response.getWriter(), new ResultResponse("Session Invalid"));
       return;
     }
+    String userId = request.getParameter("user_id");
+    double lat = Double.parseDouble(request.getParameter("lat"));
+    double lon = Double.parseDouble(request.getParameter("lon"));
+
+    Recommendation recommendation = new Recommendation();
+    List<Item> items = recommendation.recommendItems(userId, lat, lon);
+    mapper.writeValue(response.getWriter(), items);
+
 
   }
 
